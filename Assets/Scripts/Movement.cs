537f4ba32b91e4b19cdab8b09e6fb2bd642afdd1
calum-bird <calumbird3@gmail.com>
2020-02-08 19:08:12 -0400

    Add Move command

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,26 +5,29 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField]
-    private float speed;
+    private float units;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Move();
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //Move();
-        float vertical = Input.GetAxis("Vertical") * speed;
-        float horizontal = Input.GetAxis("Horizontal") * speed;
+        
+    }
 
-        // Make it move 10 meters per second instead of 10 meters per frame...
-        vertical *= Time.deltaTime;
-        horizontal *= Time.deltaTime;
-
-        // Move translation along the object's z-axis
-        transform.Translate(horizontal, vertical, 0);
+    public void Move(string direction)
+    {
+        switch(direction)
+        {
+            case "north":
+                transform.Translate(Vector3.up  * units);
+                break;
+            case "east":
+                transform.Translate(Vector3.right  * units);
+                break;
+            case "south":
+                transform.Translate(Vector3.down  * units);
+                break;
+            case "west":
+                transform.Translate(Vector3.left  * units);
+                break;
+        }
     }
 }
