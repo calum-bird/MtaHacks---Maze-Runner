c3479ec1723289938d3347559cff7ce94903d0dd
calum-bird <calumbird3@gmail.com>
2020-02-08 18:26:36 -0400

    Created PlayerSprite and began work on player movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,15 +6,36 @@ public class Movement : MonoBehaviour
 {
     [SerializeField]
     private GameObject playerObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Move(Direction.East);
+        Move(Direction.East);
+        Move(Direction.East);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void Move(Direction dir)
+    {
+        switch(dir)
+        {
+            case Direction.East:
+                playerObject.transform.Translate(Vector3.forward);
+                break;
+        }
+    }
+}
+
+public enum Direction
+{
+    East,
+    North,
+    South,
+    West
 }
